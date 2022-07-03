60dade7a53367557f9a1aa9c799604eaf4beb168
ShreedharChavan <108609745+ShreedharChavan@users.noreply.github.com>
2022-07-03 17:52:41 +0530

    Update LeadClass.java

--- LeadCreated/src/LeadClass.java
+++ LeadCreated/src/LeadClass.java
@@ -4,8 +4,7 @@ public class LeadClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Lead First Commit");
-		System.out.println("1st change");
+		System.out.println("Pull");
 
 	}
 
