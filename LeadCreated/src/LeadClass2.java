092a93ab649a682aa21416fe11136878ef024a27
Shreedhar Chavan <Shreedhar Chavan@DESKTOP-K623N0U.mshome.net>
2022-07-03 21:18:37 +0530

    sdsd

--- LeadCreated/src/LeadClass2.java
+++ LeadCreated/src/LeadClass2.java
@@ -4,7 +4,7 @@ public class LeadClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Master Change2");
+		System.out.println("Masterbbbbbbbbbbbbbb Change2");
 
 	}
 
