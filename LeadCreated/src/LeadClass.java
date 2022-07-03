484c4a300afc70ec85117333c502342cbe4f6b8d
Shreedhar Chavan <Shreedhar Chavan@DESKTOP-K623N0U.mshome.net>
2022-07-03 21:13:22 +0530

    commit

--- LeadCreated/src/LeadClass.java
+++ LeadCreated/src/LeadClass.java
@@ -4,7 +4,7 @@ public class LeadClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Master Change Testing");
+		System.out.println("mmmmmmmmmmmmm");
 
 	}
 
